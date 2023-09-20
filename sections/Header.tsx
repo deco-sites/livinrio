9097a4b83b67b4be63932ad559dcbea2972a4458
Ricardo Henrique Gonzaga <ricardo.gonzaga@agenciam3.com>
2023-09-20 20:46:24 -0300

    feat: start header

---
+++ sections/Header.tsx
@@ -0,0 +1,12 @@
+export default function Header() {
+    return (
+        <header class="h-[615px]">
+            <div class="flex flex-col items-center">
+                <div class="flex flex-col">
+                    <span class="text-[#7C6A0A] text-7xl font-medium tracking-[-7.172px]">LivinRio</span>
+                    <span class="text-[#7C6A0A] text-right text-[22px] font-normal">is fun</span>
+                </div>
+            </div>
+        </header>
+    )
+}
