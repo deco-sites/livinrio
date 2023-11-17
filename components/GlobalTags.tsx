1f3b920cced937ac9202d86cd79efcd826183852
Ricardo Henrique Gonzaga <ricardo.gonzaga@agenciam3.com>
2023-11-16 23:36:06 -0300

    perf: altera link type dos preloads das fonts

--- components/GlobalTags.tsx
+++ components/GlobalTags.tsx
@@ -8,51 +8,104 @@ function GlobalTags() {
 
       {/* Prefetch fonts */}
 
-      {/* <link
+      <link
         rel="preload"
-        type="text/css"
+        type="font/woff2"
         href={asset("/fonts/CrimsonPro-Regular.woff2")}
       />
       <link
         rel="preload"
-        type="text/css"
+        type="font/woff2"
         href={asset("/fonts/CrimsonPro-MediumItalic.woff2")}
       />
       <link
         rel="preload"
-        type="text/css"
+        type="font/woff2"
         href={asset("/fonts/CrimsonText-Bold.woff2")}
       />
       <link
         rel="preload"
-        type="text/css"
+        type="font/woff2"
         href={asset("/fonts/CrimsonText-Bold.woff2")}
       />
       <link
         rel="preload"
-        type="text/css"
+        type="font/woff2"
         href={asset("/fonts/PP-Hatton-Medium.woff2")}
       />
       <link
         as="font"
         rel="preload"
-        type="font/woff"
+        type="font/woff2"
         href={asset("/fonts/SFPRODISPLAYMEDIUM.woff2")}
       />
       <link
         as="font"
         rel="preload"
-        type="font/2"
+        type="font/woff2"
         href={asset("/fonts/SFPRODISPLAYREGULAR.woff2")}
-      /> */}
+      />
 
       <style
         dangerouslySetInnerHTML={{
           __html: `
-                html {
-                  scroll-behavior: smooth;
+          html {
+            scroll-behavior: smooth;
+          }
+                @font-face {
+                  font-family: 'Crimson Pro';
+                  font-style: normal;
+                  font-weight: 400;
+                  font-display: swap;
+                  src: url('${
+            asset("/fonts/CrimsonPro-Regular.woff2")
+          }') format('woff2');
                 }
-              
+                @font-face {
+                  font-family: 'Crimson Pro';
+                  font-style: italic;
+                  font-weight: 500;
+                  font-display: swap;
+                  src: url('${
+            asset("/fonts/CrimsonPro-MediumItalic.woff2")
+          }') format('woff2');
+                }
+                @font-face {
+                  font-family: 'Crimson Text';
+                  font-style: normal;
+                  font-weight: 700;
+                  font-display: swap;
+                  src: url('${
+            asset("/fonts/CrimsonText-Bold.woff2")
+          }') format('woff2');
+                }
+                @font-face {
+                  font-family: 'PP Hatton';
+                  font-style: normal;
+                  font-weight: 500;
+                  font-display: swap;
+                  src: url('${
+            asset("/fonts/PP-Hatton-Medium.woff2")
+          }') format('woff2');
+                }
+                @font-face {
+                  font-family: 'SF Pro Display';
+                  font-style: normal;
+                  font-weight: 500;
+                  font-display: swap;
+                  src: url('${
+            asset("/fonts/SFPRODISPLAYMEDIUM.woff2")
+          }') format('woff2');
+                }
+                @font-face {
+                  font-family: 'SF Pro Display';
+                  font-style: normal;
+                  font-weight: 400;
+                  font-display: swap;
+                  src: url('${
+            asset("/fonts/SFPRODISPLAYREGULAR.woff2")
+          }') format('woff2');
+                }
                 .swiper-container {
                   width: 100%;
                   padding-top: 36px;
